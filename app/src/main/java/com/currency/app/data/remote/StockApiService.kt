7e5dc4f69da90e9e8fb86c341e111e29349b83eb
Teddy-Hope <teddyworetaw21@gmail.com>
2026-07-22 10:44:08 +0300

    Fix: Removed legacy package attribute from AndroidManifest and cleaned StockApiService structure

--- app/src/main/java/com/currency/app/data/remote/StockApiService.kt
+++ app/src/main/java/com/currency/app/data/remote/StockApiService.kt
@@ -1,21 +1,6 @@
 package com.currency.app.data.remote
 
-import retrofit2.http.GET
-import retrofit2.http.Query
-
-interface StockApiService {
-    @GET("api/v1/quote")
-    suspend fun getStockQuote(
-        @Query("symbol") symbol: String,
-        @Query("token") token: String = BuildConfig.FINNHUB_API_KEY // 
-    ): StockQuoteDto
-}
-data class StockQuoteDto(
-    val c: Double,  // Current price 
-    val dp: Double  // Percentage change 
-)package com.currency.app.data.remote
-
-import com.currency.app.BuildConfig // 
+import com.currency.app.BuildConfig
 import retrofit2.http.GET
 import retrofit2.http.Query
 
